85c283203572332cd9735742a13e26c3dd74708c
Prasaja <prasaja55@gmail.com>
2020-12-15 12:27:12 +0700

    Background color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Ruang Baca Pras dan Rafi</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #111C29;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
